121c8a716a4f121af05d01bc37cd7ca9617f30bf
AnnaViktorovna <anna.bazileeva@gmail.com>
2024-12-11 00:40:30 -0500

    mongodb

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,27 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+require('./db/connect')
+
+app.get('/api/tasks', (req, res) => {
+    res.send('Tasks API');
+  });
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI);
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  start();

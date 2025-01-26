7a96bf0187ff96eddee565f7d0168991d197a2d2
AnnaViktorovna <anna.bazileeva@gmail.com>
2025-01-26 01:42:32 -0500

    fix errors

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -19,7 +19,7 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is listening port ${port}...`);
     });
@@ -27,6 +27,5 @@ const start = async () => {
     console.log(error);
   }
 };
-console.log('MongoDB URI:', process.env.MONGO_URI);
 
 start();

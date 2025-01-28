2b4dbbf7fb7509bb690f5233c5f8109f9d07684c
AnnaViktorovna <anna.bazileeva@gmail.com>
2025-01-28 02:38:53 -0500

    authotification with JWT Tokens

---
+++ 05-JWT-Basics/MyFolder/routes/main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+
+const { signup, login, hello } = require("../controllers/main");
+const authMiddleware = require("../middleware/auth");
+
+router.route("/hello").get(authMiddleware, hello);
+router.route("/signup").post(signup);
+router.route("/logon").post(login);
+
+module.exports = router;
